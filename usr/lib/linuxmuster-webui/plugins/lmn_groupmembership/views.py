7caf7fce1dc79ce479c3004503ba12347394765e
Andreas Till <andreas.till@netzint.de>
2018-09-12 16:44:11 +0000

    added group membership module. Still slow needs some sophomorix love

--- usr/lib/linuxmuster-webui/plugins/lmn_groupmembership/views.py
+++ usr/lib/linuxmuster-webui/plugins/lmn_groupmembership/views.py
@@ -1,18 +1,13 @@
 # coding=utf-8
-import os
 from jadi import component
 from aj.api.http import url, HttpPlugin
 from aj.api.endpoint import endpoint
 from aj.auth import authorize
-from aj.plugins.lm_common.api import lm_backup_file
 from aj.plugins.lm_common.api import lmn_getSophomorixValue
-from configparser import ConfigParser
-
-
+import subprocess
 
 @component(HttpPlugin)
 class Handler(HttpPlugin):
-
     def __init__(self, context):
         self.context = context
 
@@ -21,30 +16,44 @@ class Handler(HttpPlugin):
     @endpoint(api=True)
     def handle_api_settings(self, http_context):
         schoolname = 'default-school'
-        username = 'hulk'
-        if http_context.method == 'GET':
-            sophomorixCommand = ['sophomorix-query',  '--schoolbase', schoolname, '--user-full', '-j', '--sam', username]
-            usergroups = lmn_getSophomorixValue(sophomorixCommand, 'USER/'+username+'memberOf')
-
-            sophomorixCommand = ['sophomorix-query', '--class', '--schoolbase', schoolname, '--group-full', '-jj']
-            classes = lmn_getSophomorixValue(sophomorixCommand, 'GROUP')
-            # Parse csv config file
-            return classes
-
+        username = http_context.json_body()['username']
+        action = http_context.json_body()['action']
 
         if http_context.method == 'POST':
-            content = ''
-            data = http_context.json_body()
-            if 'admins_print' in data:
-                for k, v in self.EMAIL_MAPPING.items():
-                    data['admins_print'] = data['admins_print'].replace(k, v)
+            if action == 'list-groups':
+                memberships = {}
 
-            def convert_value(v):
-                if type(v) is int:
-                    return str(v)
-                elif type(v) is bool:
-                    return 'yes' if v else 'no'
-                else:
-                    return '%s' % v
+                # get all available groups
+                sophomorixCommand = ['sophomorix-query',  '--schoolbase', schoolname, '--user-full', '-j', '--sam', username]
+                groups = lmn_getSophomorixValue(sophomorixCommand, 'USER/'+username+'/memberOf')
+                usergroups = []
+                for group in groups:
+                    usergroups.append(group.split(',')[0].split('=')[1])
 
+                # get groups specified user is member of
+                sophomorixCommand = ['sophomorix-query', '--class', '--schoolbase', schoolname, '--group-full', '-jj']
+                schoolclasses = lmn_getSophomorixValue(sophomorixCommand, 'LISTS/GROUP')
+                #return g
+                for schoolclass in schoolclasses:
+                    memberships.setdefault(schoolclass, {})
+                    if schoolclass in usergroups:
+                        memberships[schoolclass]['member'] = True
+                    else:
+                        memberships[schoolclass]['member'] = False
+                return memberships
 
+            if action == 'set-groups':
+                groups = http_context.json_body()['groups']
+                # groupsToAdd = []
+                # groupsToRemove = []
+                # TODO Batch adding needed
+                for group in groups:
+                    if groups[group]['member'] == True:
+                        # groupsToAdd.append(group)
+                        sophomorixCommand = ['sophomorix-group',  '--addmembers', username, '--group', group]
+                        subprocess.check_call(sophomorixCommand, shell=False)
+                    if groups[group]['member'] == False:
+                        # groupsToRemove.append(group)
+                        sophomorixCommand = ['sophomorix-group',  '--removemembers', username, '--group', group]
+                        subprocess.check_call(sophomorixCommand, shell=False)
+                return 0

670419f50c648db57146f31b23feca059cb66abe
Andreas Till <andreas.till@netzint.de>
2018-09-12 16:44:11 +0000

    added group membership module. Still slow needs some sophomorix love

--- usr/lib/linuxmuster-webui/plugins/lmn_groupmembership/resources/build/all.js
+++ usr/lib/linuxmuster-webui/plugins/lmn_groupmembership/resources/build/all.js
@@ -13,13 +13,36 @@
     });
   });
 
-  angular.module('lmn.groupmembership').controller('LMNGroupMembershipController', function($scope, $http, $uibModal, gettext, notify, pageTitle, lmFileBackups) {
+  angular.module('lmn.groupmembership').controller('LMNGroupMembershipController', function($scope, $http, $uibModal, gettext, notify, pageTitle) {
     pageTitle.set(gettext('Settings'));
-    return $http.get('/api/lmn/groupmembership').then(function(resp) {
-      var school;
-      school = 'default-school';
-      console.log(resp.data);
-      return $scope.groups = resp.data;
+    $scope.getGroups = function(username) {
+      return $http.post('/api/lmn/groupmembership', {
+        action: 'list-groups',
+        username: username
+      }).then(function(resp) {
+        return $scope.groups = resp.data;
+      });
+    };
+    $scope.setGroups = function(groups) {
+      return $http.post('/api/lmn/groupmembership', {
+        action: 'set-groups',
+        username: $scope.identity.user,
+        groups: groups
+      }).then(function(resp) {
+        return notify.success(gettext('Classes enrolled'));
+      });
+    };
+    return $scope.$watch('identity.user', function() {
+      if ($scope.identity.user === void 0) {
+        return;
+      }
+      if ($scope.identity.user === null) {
+        return;
+      }
+      if ($scope.identity.user === 'root') {
+        return;
+      }
+      $scope.getGroups($scope.identity.user);
     });
   });
 
--- usr/lib/linuxmuster-webui/plugins/lmn_groupmembership/resources/js/controllers/index.controller.coffee
+++ usr/lib/linuxmuster-webui/plugins/lmn_groupmembership/resources/js/controllers/index.controller.coffee
@@ -4,11 +4,24 @@ angular.module('lmn.groupmembership').config ($routeProvider) ->
         templateUrl: '/lmn_groupmembership:resources/partial/index.html'
 
 
-angular.module('lmn.groupmembership').controller 'LMNGroupMembershipController', ($scope, $http, $uibModal, gettext, notify, pageTitle, lmFileBackups) ->
+angular.module('lmn.groupmembership').controller 'LMNGroupMembershipController', ($scope, $http, $uibModal, gettext, notify, pageTitle) ->
     pageTitle.set(gettext('Settings'))
 
 
-    $http.get('/api/lmn/groupmembership').then (resp) ->
-        school = 'default-school'
-        console.log(resp.data)
-        $scope.groups = resp.data
+    $scope.getGroups = (username) ->
+        $http.post('/api/lmn/groupmembership', {action: 'list-groups', username: username}).then (resp) ->
+            $scope.groups = resp.data
+
+    $scope.setGroups = (groups) ->
+        $http.post('/api/lmn/groupmembership', {action: 'set-groups', username:$scope.identity.user, groups: groups}).then (resp) ->
+            notify.success gettext('Classes enrolled')
+
+    $scope.$watch 'identity.user', ->
+        if $scope.identity.user is undefined
+           return
+        if $scope.identity.user is null
+           return
+        if $scope.identity.user is 'root'
+           return
+        $scope.getGroups($scope.identity.user)
+        return

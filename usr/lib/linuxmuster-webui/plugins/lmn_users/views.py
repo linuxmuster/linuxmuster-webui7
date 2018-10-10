a509d08e1b180cfb29b030cfa7638db4896fac54
Andreas Till <andreas.till@netzint.de>
2018-10-10 15:26:56 +0000

    fixed #37

--- usr/lib/linuxmuster-webui/plugins/lmn_users/views.py
+++ usr/lib/linuxmuster-webui/plugins/lmn_users/views.py
@@ -294,7 +294,8 @@ class Handler(HttpPlugin):
     @endpoint(api=True)
     def handle_api_users_apply(self, http_context):
         path = '/tmp/sophomorix.log'
-
+        # TODO: Update this function for output
+        # replace sophomorix-mode by sophomorix-update
         open(path, 'w').close()
 
         script = ''

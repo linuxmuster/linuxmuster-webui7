57c971d97bbac5821e8c5facb4b58ea8c47b90a7
kiarn <arnaud@kientz.me>
2019-02-18 11:55:44 +0100

    Quota (not finished) and password on landing page.

--- usr/lib/linuxmuster-webui/plugins/lmn_landingpage/views.py
+++ usr/lib/linuxmuster-webui/plugins/lmn_landingpage/views.py
@@ -5,7 +5,6 @@ from aj.api.endpoint import endpoint
 from aj.auth import authorize
 from aj.plugins.lm_common.api import lmn_getSophomorixValue
 
-
 @component(HttpPlugin)
 class Handler(HttpPlugin):
     def __init__(self, context):
@@ -19,3 +18,13 @@ class Handler(HttpPlugin):
         username = http_context.json_body()['username']
         action = http_context.json_body()['action']
 
+    @url(r'/api/lmn/quota/(?P<user>\w+)') ## TODO authorize
+    @endpoint(api=True)
+    def handle_api_quota(self, http_context, user):
+        
+        v = {'total': 2147483648, 'used':62914560} ## TODO how to get values from sophomorix-quota
+        return {
+            'used': v['used'],
+            'free': v['total']-v['used'],
+            'total': v['total']
+        }

500bda52f024f22c6399eb215c91015c8d665bf4
Andreas Till <andreas.till@netzint.de>
2019-03-12 07:52:44 +0000

    quick fix: landingpage if no quota limitation is set

--- usr/lib/linuxmuster-webui/plugins/lmn_landingpage/views.py
+++ usr/lib/linuxmuster-webui/plugins/lmn_landingpage/views.py
@@ -5,6 +5,7 @@ from aj.api.endpoint import endpoint
 from aj.auth import authorize
 from aj.plugins.lmn_common.api import lmn_getSophomorixValue
 
+
 @component(HttpPlugin)
 class Handler(HttpPlugin):
     def __init__(self, context):
@@ -18,22 +19,31 @@ class Handler(HttpPlugin):
         username = http_context.json_body()['username']
         action = http_context.json_body()['action']
 
-    @url(r'/api/lmn/quota/') ## TODO authorize
+    @url(r'/api/lmn/quota/')  ## TODO authorize
     @endpoint(api=True)
     def handle_api_quota(self, http_context):
+
        if http_context.method == 'POST':
-                
+
                 user = self.context.identity
-                
+
                 if user != 'root':
                     sophomorixCommand = ['sophomorix-query', '--sam', user, '--user-full', '--quota-usage', '-jj']
                     jsonpath          = 'USER/' + user + '/QUOTA_USAGE_BY_SHARE/linuxmuster-global'
                     result            = lmn_getSophomorixValue(sophomorixCommand, jsonpath)
-                    return {
-                        'used': result['USED_MiB'],
-                        'free': result['HARD_LIMIT_MiB']-result['USED_MiB'],
-                        'total': result['HARD_LIMIT_MiB']
-                    }
+                    if result['HARD_LIMIT_MiB'] == 'NO LIMIT':
+                        return {
+                            'used': result['USED_MiB'],
+                            'free':  'Unlimited',
+                            'total': 'Unlimited'
+                        }
+                    else:
+
+                        return {
+                            'used': result['USED_MiB'],
+                            'free': result['HARD_LIMIT_MiB']-result['USED_MiB'],
+                            'total': result['HARD_LIMIT_MiB']
+                        }
                 else:
                     return {
                         'used': 0,

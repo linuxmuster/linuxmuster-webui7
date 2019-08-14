bb4b60f60583bc115cb2e1a89c2064390bd6ea27
Andreas Till <andreas.till@netzint.de>
2019-08-14 15:21:32 +0000

    merged crowdin translation

--- usr/lib/linuxmuster-webui/plugins/lmn_printers/locale/th/LC_MESSAGES/app.js
+++ usr/lib/linuxmuster-webui/plugins/lmn_printers/locale/th/LC_MESSAGES/app.js
@@ -1,1 +1,1 @@
-{"": "", "{{printer.name}}": "", "Saved": "", "No filter is set. All devices can use this printer.": "", "Add": "", "Configure printers": "", "Printers": "", "Close": "", "Save": "", "Type to search...": ""}
+{"": "", "{{printer.name}}": "", "Saved": "", "Printers": "", "No filter is set. All devices can use this printer.": "", "Add": "", "Configure printers": "", "Close": "", "Save": "", "Type to search...": ""}

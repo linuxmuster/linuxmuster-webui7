5909f0adb2dae26c1e01aca1a101c61d22b41b9f
kiarn <arnaud@kientz.me>
2019-12-16 23:08:30 +0100

    Update translations list and french translations.

--- usr/lib/linuxmuster-webui/plugins/lmn_quotas/views.py
+++ usr/lib/linuxmuster-webui/plugins/lmn_quotas/views.py
@@ -87,7 +87,7 @@ class Handler(HttpPlugin):
             groups = {'adminclass':{}, 'project':{}}
             shares = ['linuxmuster-global', 'default-school']
 
-            sophomorixCommand = ['sophomorix-class', '-ij']
+            sophomorixCommand = ['sophomorix-class', '-i', '-jj']
             result = lmn_getSophomorixValue(sophomorixCommand, 'GROUPS')
 
             for group, details in result.items():

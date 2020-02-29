96165f8168d3ae6719131db7dc5daa4c152fda97
kiarn <arnaud@kientz.me>
2020-02-29 13:12:11 +0100

    Avoid "dict change length error": keys is an iterator in PY3.

--- usr/lib/linuxmuster-webui/plugins/lmn_users/views.py
+++ usr/lib/linuxmuster-webui/plugins/lmn_users/views.py
@@ -397,7 +397,7 @@ class Handler(HttpPlugin):
         results = lmn_getSophomorixValue(sophomorixCommand, '')
         ## Remove UPDATE entries which are also in KILL ( necessary to show it in KILL and UPDATE ? )
         if "UPDATE" in results["CHECK_RESULT"] and "KILL" in results["CHECK_RESULT"]:
-            for user_update in results["CHECK_RESULT"]["UPDATE"].keys():
+            for user_update in tuple(results["CHECK_RESULT"]["UPDATE"]):
                 if user_update in results["CHECK_RESULT"]["KILL"]:
                     del results["CHECK_RESULT"]["UPDATE"][user_update]
         return results

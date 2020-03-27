f92fb470cf5619f2d35488a16f4f8885d97e730c
kiarn <arnaud@kientz.me>
2020-03-27 23:11:25 +0100

    Fix KeyError for sophomorixStatus.

--- usr/lib/linuxmuster-webui/plugins/lmn_users/views.py
+++ usr/lib/linuxmuster-webui/plugins/lmn_users/views.py
@@ -232,7 +232,7 @@ class Handler(HttpPlugin):
                 with authorize('lm:users:teachers:list'):
                     # TODO: This could run with --user-basic but not all memberOf are filled. Needs verification
                     # Not so pretty to list teachers, --user-minimal with JSON output would be better
-                    sophomorixCommand = ['sophomorix-query', '--teacher', '--schoolbase', schoolname, '--user-basic', '-jj']
+                    sophomorixCommand = ['sophomorix-query', '--teacher', '--schoolbase', schoolname, '--user-full', '-jj']
             else:
                 with authorize('lm:users:teachers:read'):
                     user = http_context.json_body()['user']

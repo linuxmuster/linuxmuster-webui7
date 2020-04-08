ad4e15ff423851dc27508810236e72d4f9b6ef51
Andreas Till <andreas.till@netzint.de>
2020-04-08 10:39:39 +0200

    fixed some python3 related packages and split_config

--- usr/lib/linuxmuster-webui/etc/split_config_aj.py
+++ usr/lib/linuxmuster-webui/etc/split_config_aj.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import yaml
 import shutil
@@ -12,15 +12,15 @@ config_webui = {}
 
 if not os.path.isfile(path_webui + config_file):
     try:
-	if not os.path.isdir(path_webui):
-		os.mkdir(path_webui, 0755)
-        ## Backup 
-        shutil.copyfile(path_ajenti + config_file, path_ajenti + config_file + '.bak')
+        if not os.path.isdir(path_webui):
+            os.mkdir(path_webui, 755)
+            ## Backup
+            shutil.copyfile(path_ajenti + config_file, path_ajenti + config_file + '.bak')
 
-        ## Load existing config
-        print("Load existing ajenti config file ...")
-        with open(path_ajenti + config_file, 'r') as f:
-            config_ajenti = yaml.load(f, Loader=yaml.Loader)
+            ## Load existing config
+            print("Load existing ajenti config file ...")
+            with open(path_ajenti + config_file, 'r') as f:
+                config_ajenti = yaml.load(f, Loader=yaml.Loader)
     except:
         print("Error loading ajenti config file ...")
         sys.exit()
@@ -28,16 +28,15 @@ if not os.path.isfile(path_webui + config_file):
     ## Copy linuxmuster config
     print("Copy linuxmuster config in /etc/linuxmuster/webui/config.yml ...")
     try:
-    	config_webui['linuxmuster'] = config_ajenti['linuxmuster']
+        config_webui['linuxmuster'] = config_ajenti['linuxmuster']
         ## Update ajenti config
         print("Update ajenti config ...")
         del config_ajenti['linuxmuster']
     except:
-	print("Linuxmuster key not found ! Please check the file /etc/linuxmuster/webui/config.yml !")
+        print("Linuxmuster key not found ! Please check the file /etc/linuxmuster/webui/config.yml !")
 
     with open(path_webui + config_file, 'w') as f:
         yaml.dump(config_webui, f, default_flow_style=False)
-
 
     with open(path_ajenti + config_file, 'w') as f:
         yaml.dump(config_ajenti, f, default_flow_style=False)

df4b9a499edbdf17b2e783979058e49f5ef7c0b9
kiarn <arnaud@kientz.me>
2020-04-19 16:57:26 +0200

    Temporary fix for linbo icon download.

--- usr/lib/linuxmuster-webui/plugins/lmn_linbo/views.py
+++ usr/lib/linuxmuster-webui/plugins/lmn_linbo/views.py
@@ -101,7 +101,7 @@ class Handler(HttpPlugin):
 
         if not path.startswith(root):
             return http_context.respond_forbidden()
-        return http_context.file(path, inline=False, name=name)
+        return http_context.file(path, inline=False, name=name.encode())
 
     @url(r'/api/lm/linbo/image/(?P<name>.+)')
     @authorize('lm:linbo:images')

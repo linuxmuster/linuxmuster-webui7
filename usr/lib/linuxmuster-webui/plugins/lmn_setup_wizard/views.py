7f77438025b99d7ae5f31f38085e5d0ec10c8b90
kiarn <arnaud@kientz.me>
2021-01-02 12:30:54 +0100

    Docstrings setup wizard.

--- usr/lib/linuxmuster-webui/plugins/lmn_setup_wizard/views.py
+++ usr/lib/linuxmuster-webui/plugins/lmn_setup_wizard/views.py
@@ -1,3 +1,7 @@
+"""
+Handles `setup.ini` file and finish the install process.
+"""
+
 import configparser
 import os
 import subprocess
@@ -14,7 +18,17 @@ class Handler(HttpPlugin):
 
     @url(r'/api/lm/setup-wizard/read-ini')
     @endpoint(api=True, auth=True)
-    def handle_api_read_log(self, http_context, path=None):
+    def handle_api_read_log(self, http_context):
+        """
+        Read settings from `/tmp/setup.ini`.
+        Method GET.
+
+        :param http_context: HttpContext
+        :type http_context: HttpContext
+        :return: All setup settings
+        :rtype: dict
+        """
+
         config = configparser.RawConfigParser()
         if http_context.method == 'GET':
             if os.path.exists('/tmp/setup.ini'):
@@ -39,7 +53,15 @@ class Handler(HttpPlugin):
 
     @url(r'/api/lm/setup-wizard/update-ini')
     @endpoint(api=True, auth=True)
-    def handle_api_log(self, http_context, path=None):
+    def handle_api_log(self, http_context):
+        """
+        Write setup config into `/tmp/setup.ini`.
+        Method POST.
+
+        :param http_context: HttpContext
+        :type http_context: HttpContext
+        """
+
         cfg = configparser.RawConfigParser()
         if os.path.exists('/tmp/setup.ini'):
             cfg.read('/tmp/setup.ini')
@@ -63,11 +85,28 @@ class Handler(HttpPlugin):
     @url(r'/api/lm/setup-wizard/is-configured')
     @endpoint(api=True)
     def handle_api_is_configured(self, http_context):
+        """
+        Test if linuxmuster was already configured by checking if the file
+        `/var/lib/linuxmuster/setup.ini` exists.
+
+        :param http_context: HttpContext
+        :type http_context: HttpContext
+        :return: Existence of `setup.ini`
+        :rtype: bool
+        """
+
         return os.path.exists('/var/lib/linuxmuster/setup.ini')
 
     @url(r'/api/lm/setup-wizard/provision')
     @endpoint(api=True, auth=True)
     def handle_api_provision(self, http_context):
+        """
+        Launch `linuxmuster-setup` to configure all services.
+
+        :param http_context: HttpContext
+        :type http_context: HttpContext
+        """
+
         if http_context.method != 'POST':
             return
         if http_context.json_body()['start'] == 'setup':
@@ -82,6 +121,13 @@ class Handler(HttpPlugin):
     @url(r'/api/lm/setup-wizard/restart') ## TODO : Handle maybe obsolet
     @endpoint(api=True, auth=True)
     def handle_api_restart(self, http_context):
+        """
+        Restart webui service after setup is finished (to switch to https).
+
+        :param http_context: HttpContext
+        :type http_context: HttpContext
+        """
+
         if http_context.method != 'POST':
             return
         try:

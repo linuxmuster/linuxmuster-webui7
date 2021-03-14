a0ad3f9b5780dd3db04b36cc853297a2a4b35732
kiarn <arnaud@kientz.me>
2021-03-14 21:02:53 +0100

    Only allow plugin permission in dev mode.

--- usr/lib/linuxmuster-webui/plugins/lmn_permissions/main.py
+++ usr/lib/linuxmuster-webui/plugins/lmn_permissions/main.py
@@ -2,21 +2,22 @@ from jadi import component
 
 from aj.auth import PermissionProvider
 from aj.plugins.core.api.sidebar import SidebarItemProvider
+import aj
 
+if aj.dev:
+    @component(SidebarItemProvider)
+    class ItemProvider(SidebarItemProvider):
+        def __init__(self, context):
+            self.context = context
 
-@component(SidebarItemProvider)
-class ItemProvider(SidebarItemProvider):
-    def __init__(self, context):
-        self.context = context
-
-    def provide(self):
-        return [
-            {
-                'attach': 'category:tools',
-                'id': 'permissions',
-                'name': _('Permissions'),
-                'icon': 'fas fa-user-lock',
-                'url': '/view/permissions',
-                'children': [],
-            }
-        ]
+        def provide(self):
+            return [
+                {
+                    'attach': 'category:tools',
+                    'id': 'permissions',
+                    'name': _('Permissions'),
+                    'icon': 'fas fa-user-lock',
+                    'url': '/view/permissions',
+                    'children': [],
+                }
+            ]

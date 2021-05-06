137c97edf02d64b614dfaa0c298cd1c33f8df34b
Andreas Till <andreas.till@netzint.de>
2021-05-06 16:07:11 +0000

    fix webuUpload ntacl correctly

--- usr/lib/linuxmuster/setup.d/w_webui.py
+++ usr/lib/linuxmuster/setup.d/w_webui.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 import subprocess
 
+print("* Create Webui Configuration")
+
 subprocess.run(['/usr/lib/linuxmuster-webui/etc/create_aj_cfg.sh'])
+
+print("* Create Webui Upload Folder")
+subprocess.run(['/usr/lib/linuxmuster-webui/etc/create_webuiUploadFolder'])
+
 print("* WebUI Setup Success!")

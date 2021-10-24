cc5a7fd3ed28d9c512fb9151829a66ac75199cda
kiarn <arnaud@kientz.me>
2021-10-24 11:56:47 +0200

    Ignore docs and tests directories for packaging.

--- buildpackage.sh
+++ buildpackage.sh
@@ -2,4 +2,4 @@
 
 # build the debian package
 
-dpkg-buildpackage -rfakeroot -tc -sa -us -uc -I".directory" -I".git" -I"buildpackage.sh" -I".crowdin.key"
+dpkg-buildpackage -rfakeroot -tc -sa -us -uc -I".directory" -I".git" -I"buildpackage.sh" -I".crowdin.key" -I"docs" -I"docs_src" -I"tests"

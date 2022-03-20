dd319a8fb47d798393c0fc308feb1a77abc4f1b4
kiarn <arnaud@kientz.me>
2022-03-20 21:04:22 +0100

    Breadcrumb.

--- usr/lib/linuxmuster-webui/plugins/lmn_home/resources/build/all.js
+++ usr/lib/linuxmuster-webui/plugins/lmn_home/resources/build/all.js
@@ -19,26 +19,51 @@ angular.module('lmn.home').controller('HomeIndexController', function ($scope, $
     pageTitle.set('path', $scope);
 
     $scope.loading = true;
-    $scope.root = false;
 
     identity.promise.then(function () {
         if (identity.user == 'root') {
-            $scope.root = true;
             $scope.loading = false;
         } else {
             $scope.home = identity.profile.homeDirectory;
             $scope.load_path($scope.home);
+            $scope.splitted_path = [];
         }
     });
 
     $scope.load_path = function (path) {
+        $scope.splitted_path_items = path.replace($scope.home, '').split('/');
+        $scope.splitted_path = [];
+        progressive_path = $scope.home;
+        var _iteratorNormalCompletion = true;
+        var _didIteratorError = false;
+        var _iteratorError = undefined;
+
+        try {
+            for (var _iterator = $scope.splitted_path_items[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                item = _step.value;
+
+                if (item != '') {
+                    progressive_path = progressive_path + '/' + item;
+                    $scope.splitted_path.push({ 'path': progressive_path, 'name': item });
+                }
+            }
+        } catch (err) {
+            _didIteratorError = true;
+            _iteratorError = err;
+        } finally {
+            try {
+                if (!_iteratorNormalCompletion && _iterator.return) {
+                    _iterator.return();
+                }
+            } finally {
+                if (_didIteratorError) {
+                    throw _iteratorError;
+                }
+            }
+        }
+
         samba_share.list(path).then(function (data) {
             $scope.items = data.items;
-            if (path == $scope.home) {
-                $scope.parent = '';
-            } else {
-                $scope.parent = $scope.home;
-            }
         }, function (data) {
             notify.error(gettext('Could not load directory'), data.message);
         }).finally(function () {
--- usr/lib/linuxmuster-webui/plugins/lmn_home/resources/js/controllers/index.controller.es
+++ usr/lib/linuxmuster-webui/plugins/lmn_home/resources/js/controllers/index.controller.es
@@ -2,28 +2,31 @@ angular.module('lmn.home').controller('HomeIndexController', function($scope, $r
     pageTitle.set('path', $scope);
 
     $scope.loading = true;
-    $scope.root = false;
 
     identity.promise.then(() => {
         if (identity.user == 'root') {
-            $scope.root = true;
             $scope.loading = false;
         }
         else {
             $scope.home = identity.profile.homeDirectory;
             $scope.load_path($scope.home);
+            $scope.splitted_path = [];
         }
     });
 
     $scope.load_path = (path) => {
+        $scope.splitted_path_items = path.replace($scope.home, '').split('/');
+        $scope.splitted_path = [];
+        progressive_path = $scope.home;
+        for (item of $scope.splitted_path_items) {
+            if (item != '') {
+                progressive_path = progressive_path + '/' + item;
+                $scope.splitted_path.push({'path': progressive_path, 'name': item});
+            }
+        }
+
         samba_share.list(path).then((data) => {
                 $scope.items = data.items;
-                if (path == $scope.home) {
-                    $scope.parent = '';
-                }
-                else {
-                    $scope.parent = $scope.home;
-                }
             }, (data) => {
                 notify.error(gettext('Could not load directory'), data.message)
             }).finally(() => {

95c6b3fc713ebfa740732fc99c20952d52a79eed
kiarn <arnaud@kientz.me>
2023-01-26 17:48:43 +0100

    WebdavXMLResponse as object.

--- usr/lib/linuxmuster-webui/plugins/lmn_smbclient/davxml.py
+++ usr/lib/linuxmuster-webui/plugins/lmn_smbclient/davxml.py
@@ -1,7 +1,6 @@
 import lxml.etree as etree
 
-
-def xml_propfind_response(r):
+class WebdavXMLResponse:
     valid_properties = {
         # 'resourcetype', # --> collection or not
         'getlastmodified',
@@ -18,23 +17,35 @@ def xml_propfind_response(r):
     # - lockdiscovery
     # - supportedlock
 
-    root = etree.Element("{DAV:}multistatus", nsmap={"d": "DAV:"})
+    def __init__(self):
+        pass
 
-    for path, details in r.items():
-        response = etree.SubElement(root, "{DAV:}response")
-        href = etree.SubElement(response, "{DAV:}href")
-        href.text = path
-        propstat_200 = etree.SubElement(response, "{DAV:}propstat")
-        propstat_404 = etree.SubElement(response, "{DAV:}propstat")
+    def make_propfind_response(self, items):
+        xml_root = etree.Element("{DAV:}multistatus", nsmap={"d": "DAV:"})
+        for href,properties in items.items():
+            xml_response = etree.SubElement(xml_root, "{DAV:}response")
+            xml_href = etree.SubElement(xml_response, "{DAV:}href")
+            xml_href.text = href
+            self._make_item_propstat(xml_response, properties)
+
+        return etree.tostring(xml_root).decode()
+
+    def _make_item_propstat(self, xml_response, properties):
+        propstat_200 = etree.SubElement(xml_response, "{DAV:}propstat")
+        propstat_404 = etree.SubElement(xml_response, "{DAV:}propstat")
+
         prop_200 = etree.SubElement(propstat_200, "{DAV:}prop")
         prop_404 = etree.SubElement(propstat_404, "{DAV:}prop")
+
         status_200 = etree.SubElement(propstat_200, "{DAV:}status")
         status_404 = etree.SubElement(propstat_404, "{DAV:}status")
+
         status_200.text = "HTTP/1.1 200 OK"
         status_404.text = "HTTP/1.1 404 Not Found"
 
-        for prop in valid_properties:
-            value = details.get(prop, None)
+        # TODO: replace valid_properties with requested prop
+        for prop in self.valid_properties:
+            value = properties.get(prop, None)
             if value is None:
                 etree.SubElement(prop_404, f"{{DAV:}}{prop}")
             else:
@@ -42,8 +53,5 @@ def xml_propfind_response(r):
                 prop.text = value
 
         isdir = etree.SubElement(prop_200, f"{{DAV:}}resourcetype")
-        if details['isDir']:
+        if properties['isDir']:
             etree.SubElement(isdir, f"{{DAV:}}collection")
-
-
-    return etree.tostring(root).decode()

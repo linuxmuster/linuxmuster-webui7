1e406cae82eae82df8fc94aca36d2066b9230d0d
kiarn <arnaud@kientz.me>
2023-03-27 18:35:24 +0200

    Add dataclass schoolclass model.

--- usr/lib/linuxmuster-webui/plugins/lmn_common/ldap/models.py
+++ usr/lib/linuxmuster-webui/plugins/lmn_common/ldap/models.py
@@ -1,6 +1,34 @@
 from dataclasses import dataclass
 from datetime import datetime
 
+
+@dataclass
+class LMNSchoolClass:
+    cn: str
+    description: str
+    distinguishedName: str
+    mail: list
+    member: list
+    memberOf: list
+    name: str
+    sAMAccountName: str
+    sAMAccountType: str
+    sophomorixAddMailQuota: list
+    sophomorixAddQuota: list
+    sophomorixAdmins: list
+    sophomorixCreationDate: datetime
+    sophomorixHidden: bool
+    sophomorixJoinable: bool
+    sophomorixMailAlias: list
+    sophomorixMailList: list
+    sophomorixMailQuota: list
+    sophomorixMaxMembers: int
+    sophomorixMembers: list
+    sophomorixQuota: list
+    sophomorixSchoolname: str
+    sophomorixStatus: str
+    sophomorixType: str
+
 @dataclass
 class LMNUser:
     cn: str

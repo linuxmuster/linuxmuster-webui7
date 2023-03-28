a65418db491f409cbd220eebe1b1bf960d76c8d9
kiarn <arnaud@kientz.me>
2023-03-28 17:40:48 +0200

    Extract specific usual groups.

--- usr/lib/linuxmuster-webui/plugins/lmn_common/ldap/models.py
+++ usr/lib/linuxmuster-webui/plugins/lmn_common/ldap/models.py
@@ -2,6 +2,40 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import List
 
+def extract_name(dn):
+    # 'CN=11c,OU=11c,OU=Students,OU=default-school,OU=SCHOOLS...' becomes :
+    # [[('CN', '11c', 1)], [('OU', '11c', 1)], [('OU', 'Students', 1)],...]
+    try:
+        return ldap.dn.str2dn(dn)[0][0][1]
+    except KeyError:
+        return ''
+
+def extract_schoolclasses(membership):
+    schoolclasses = []
+    for dn in membership:
+        if 'OU=Students' in dn:
+            schoolclass = extract_name(dn)
+            if schoolclass:
+                schoolclasses.append(schoolclass)
+    return schoolclasses
+
+def extract_projects(membership):
+    projects = []
+    for dn in membership:
+        if 'OU=Projects' in dn:
+            project = extract_name(dn)
+            if project:
+                projects.append(project)
+    return projects
+
+def extract_management(membership):
+    management = []
+    for dn in membership:
+        if 'OU=Management' in dn:
+            group = extract_name(dn)
+            if group in ['internet', 'intranet', 'printing', 'webfilter', 'wifi']:
+                management.append(group)
+    return management
 
 @dataclass
 class LMNProject:
@@ -121,4 +155,9 @@ class LMNUser:
     sophomorixWebuiDashboard: list
     sophomorixWebuiPermissionsCalculated: list
     unixHomeDirectory: str
+
+    def __post_init__(self):
+        self.schoolclasses = extract_schoolclasses(self.memberOf)
+        self.projects = extract_projects(self.memberOf)
+        self.management = extract_management(self.memberOf)
 

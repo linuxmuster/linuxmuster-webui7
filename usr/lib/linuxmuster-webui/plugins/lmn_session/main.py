711feb8f407b96992942a95f10241f75809a98e1
kiarn <arnaud@kientz.me>
2023-04-17 20:03:13 +0200

    Preserve old session until the new one is published.

--- usr/lib/linuxmuster-webui/plugins/lmn_session/main.py
+++ usr/lib/linuxmuster-webui/plugins/lmn_session/main.py
@@ -1,5 +1,7 @@
 from jadi import component
 from aj.plugins.core.api.sidebar import SidebarItemProvider
+from aj.auth import PermissionProvider
+
 
 
 @component(SidebarItemProvider)
@@ -16,5 +18,28 @@ class ItemProvider(SidebarItemProvider):
             'url': '/view/lmn/oldsession',
             'weight': 15,
             },
+            {
+            'attach': 'category:class',
+            'name': _('Print Passwords'),
+            'icon': 'key',
+            'url': '/view/lmn/users/print-passwords',
+            'weight': 40,
+            }
         ]
 
+
+@component(PermissionProvider)
+class Permissions (PermissionProvider):
+    def provide(self):
+        return [
+            {
+                'id': 'lm:users:teachers:write',
+                'name': _('Write students'),
+                'default': False,
+            },
+            {
+                'id': 'lmn:oldsession:trans',
+                'name': _('Transfer Files in Session'),
+                'default': False,
+            },
+        ]

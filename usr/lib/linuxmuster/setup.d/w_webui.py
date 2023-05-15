9332a9fdd69cfcf64cc8bf964a36a49b772a5a56
Andreas Till <andreas.till@netzint.de>
2023-05-15 19:10:19 +0200

    Added another fix for .installed flag in setup process

--- usr/lib/linuxmuster/setup.d/w_webui.py
+++ usr/lib/linuxmuster/setup.d/w_webui.py
@@ -8,4 +8,7 @@ subprocess.run(['/usr/lib/linuxmuster-webui/etc/install_scripts/create_aj_cfg.sh
 print("* Create Webui Upload Folder")
 subprocess.run(['/usr/lib/linuxmuster-webui/etc/install_scripts/create_webuiUploadFolder.sh'])
 
+print("* Create Webui Installed Flag")
+subprocess.run(['touch', '/usr/lib/linuxmuster-webui/etc/.installed'])
+
 print("* WebUI Setup Success!")

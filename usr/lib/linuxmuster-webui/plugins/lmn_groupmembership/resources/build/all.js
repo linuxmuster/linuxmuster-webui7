e3b77ab197f355344be9b29a5bf62a85520d25e4
kiarn <arnaud@kientz.me>
2023-11-25 14:47:14 +0100

    Ask again to renew kerberos ticket.

--- usr/lib/linuxmuster-webui/plugins/lmn_groupmembership/resources/build/all.js
+++ usr/lib/linuxmuster-webui/plugins/lmn_groupmembership/resources/build/all.js
@@ -13,7 +13,7 @@
     });
   });
 
-  angular.module('lmn.groupmembership').controller('LMNGroupMembershipController', function($rootScope, $scope, $http, identity, $uibModal, gettext, notify, pageTitle, messagebox, validation, smbclient) {
+  angular.module('lmn.groupmembership').controller('LMNGroupMembershipController', function($rootScope, $scope, $http, $window, identity, $uibModal, gettext, notify, pageTitle, messagebox, validation, smbclient) {
     $scope.need_krbcc_refresh = false;
     pageTitle.set(gettext('Enrolle'));
     $scope.show_schoolclasses = true;
@@ -22,6 +22,19 @@
     $scope.loading_schoolclasses = true;
     $scope.loading_projects = true;
     $scope.loading_printers = true;
+    $scope.$on("$destroy", function() {
+      var warning;
+      warning = "These changes can only take effect if the session is renewed, otherwise it will be active after the next logout/login. It could be useful for the session plugin to do it now.";
+      if ($scope.need_krbcc_refresh) {
+        return messagebox.show({
+          text: warning,
+          positive: 'Do it now',
+          negative: 'Later'
+        }).then(function() {
+          return $scope.refresh_krbcc();
+        });
+      }
+    });
     $scope.sorts = [
       {
         name: gettext('Groupname'),
@@ -55,7 +68,7 @@
       $scope.changeState = true;
       sophomorix_type_map = {
         'printer': 'group',
-        'schoolclass': 'class',
+        'adminclass': 'class',
         'project': 'project'
       };
       if (group.sophomorixType === 'printer') {

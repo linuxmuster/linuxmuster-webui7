39e930c3d1d7488cd07112f831dfde91288b2232
kiarn <arnaud@kientz.me>
2023-12-30 11:36:36 +0100

    Add tags to enable and disable sessions plugins.

--- usr/lib/linuxmuster-webui/plugins/lmn_session_new/__init__.py
+++ usr/lib/linuxmuster-webui/plugins/lmn_session_new/__init__.py
@@ -1,2 +1,22 @@
-from .main import *
-from .views import *
+import os
+import logging
+
+if os.path.isfile('/etc/linuxmuster/webui/disable_new_session_plugin'):
+    try:
+        with open('/etc/linuxmuster/webui/disable_new_session_plugin') as tag_file:
+            tag = tag_file.read()[0]
+        if tag == '1':
+            logging.info("Disabling new session plugin")
+        else:
+            logging.warning("/etc/linuxmuster/webui/disable_new_session_plugin does not contain a valid tag (should be 1)")
+            logging.info('New session is enabled')
+            from .main import *
+            from .views import *
+    except IndexError as e:
+        logging.warning("/etc/linuxmuster/webui/disable_new_session_plugin does not contain a valid tag (is empty)")
+        logging.info('New session is enabled')
+        from .main import *
+        from .views import *
+else:
+    from .main import *
+    from .views import *

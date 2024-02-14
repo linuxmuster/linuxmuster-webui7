36e062560ff079c1d9d1b1cb40fc041e1d221052
kiarn <arnaud@kientz.me>
2024-02-14 14:00:30 +0100

    Avoid setting ACLs on webuiUpload when not configured.

--- usr/lib/linuxmuster-webui/etc/install_scripts/create_webuiUploadFolder.sh
+++ usr/lib/linuxmuster-webui/etc/install_scripts/create_webuiUploadFolder.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-# check if samba already installed
+
+# Check if samba is already installed
 installed=$(grep active\ directory\ domain\ controller /etc/samba/smb.conf)
 ACLS="/usr/lib/linuxmuster-webui/etc/install_scripts/webuiUpload.ntacl"
 
-if [ -d "/srv/webuiUpload/" ];then
+if [ -d "/srv/webuiUpload/" ]; then
     rm -R /srv/webuiUpload/
 fi
 
 mkdir -p /srv/webuiUpload/
-if [ -z "$installed" ];then
-    echo "not installed skip setting acl on webuiUpload"
+if [ -z "$installed" ]; then
+    echo "Samba is not installed, skip setting acl on webuiUpload."
+elif [ ! -e "/var/lib/linuxmuster/setup.ini" ]; then
+    echo "linuxmuster.net is not configured, skip setting acl on webuiUpload."
 else
     workgroup=$(testparm -l -s --parameter-name=workgroup 2>/dev/null)
     sed -i "s/LINUXMUSTER/$workgroup/" $ACLS
     setfacl --set-file $ACLS /srv/webuiUpload
+    echo "ACl set on webuiUpload with WORKGROUP $workgroup"
 fi

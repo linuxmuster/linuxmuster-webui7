cb2ea0353cd5b7917fd40acef07098a7f86c4971
kiarn <arnaud@linuxmuster.net>
2025-01-13 12:45:36 +0100

    Update documentation.

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '7.2.14',
+    VERSION: '7.2.71',
     LANGUAGE: 'en_GB',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

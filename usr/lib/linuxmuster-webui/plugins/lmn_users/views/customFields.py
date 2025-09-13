9fb5264d95672183632e17040e32a68bdff72fc6
kiarn <arnaud@linuxmuster.net>
2025-09-13 18:19:03 +0200

    Use profile in context.

--- usr/lib/linuxmuster-webui/plugins/lmn_users/views/customFields.py
+++ usr/lib/linuxmuster-webui/plugins/lmn_users/views/customFields.py
@@ -7,7 +7,7 @@ import os
 from jadi import component
 from aj.api.http import get, post, patch, HttpPlugin
 from aj.api.endpoint import endpoint, EndpointError, EndpointReturn
-from aj.auth import authorize, AuthenticationService
+from aj.auth import authorize
 from aj.plugins.lmn_common.api import lmn_getSophomorixValue
 
 
@@ -160,7 +160,7 @@ class Handler(HttpPlugin):
         if user != 'root':
             custom_fields = self.context.schoolmgr.custom_fields
             custom_fields_to_show = []
-            profil = AuthenticationService.get(self.context).get_provider().get_profile(user)
+            profil = self.context.profile
             role = profil['sophomorixRole'] + 's'
 
             for field in ['custom', 'customMulti', 'proxyAddresses']:

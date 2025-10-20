9081c9ee4e0b71b0ff310cdfa30c83420f753a12
kiarn <arnaud@linuxmuster.net>
2025-10-20 22:11:13 +0200

    Catch index errors on quotas.

--- usr/lib/linuxmuster-webui/plugins/lmn_quotas/views.py
+++ usr/lib/linuxmuster-webui/plugins/lmn_quotas/views.py
@@ -39,14 +39,18 @@ class Handler(HttpPlugin):
             return {}
 
         if user != 'root':
-            sophomorixCommand = ['sophomorix-query', '--sam', user, '--user-full', '--quota-usage', '-jj']
-            jsonpath = 'USER/' + user
-            data = lmn_getSophomorixValue(sophomorixCommand, jsonpath)
-            return {
-                'QUOTA_USAGE_BY_SHARE': data['QUOTA_USAGE_BY_SHARE'],
-                'sophomorixCloudQuotaCalculated': data['sophomorixCloudQuotaCalculated'],
-                'sophomorixMailQuotaCalculated': data['sophomorixMailQuotaCalculated'],
-            }
+            try:
+                sophomorixCommand = ['sophomorix-query', '--sam', user, '--user-full', '--quota-usage', '-jj']
+                jsonpath = 'USER/' + user
+                data = lmn_getSophomorixValue(sophomorixCommand, jsonpath)
+                return {
+                    'QUOTA_USAGE_BY_SHARE': data['QUOTA_USAGE_BY_SHARE'],
+                    'sophomorixCloudQuotaCalculated': data['sophomorixCloudQuotaCalculated'],
+                    'sophomorixMailQuotaCalculated': data['sophomorixMailQuotaCalculated'],
+                }
+            except IndexError:
+                return {}
+
         return {}
 
     @get(r'/api/lmn/quota/usermap/(?P<user>[a-z0-9\-_]*)')

fe56ee0751070e29f73de6046672cca693891b54
kiarn <arnaud@linuxmuster.net>
2025-11-19 18:12:55 +0100

    Bring back dot in passwords.

--- usr/lib/linuxmuster-webui/plugins/lmn_common/resources/build/all.js
+++ usr/lib/linuxmuster-webui/plugins/lmn_common/resources/build/all.js
@@ -453,7 +453,7 @@ angular.module('core').directive('teacherAccess', function (identity) {
     this.isStrongPwd = function(password) {
       var error_msg, regExp, validPassword;
       error_msg = gettext('Passwords must contain at least one lowercase, one uppercase, one special char or number, and at least 7 chars');
-      regExp = /(?=.*[a-z])(?=.*[A-Z])(?=.*[?!@#§+\-$%&*{}()]|(?=.*\d)).{7,}/;
+      regExp = /(?=.*[a-z])(?=.*[A-Z])(?=.*[?!@#§+\.\-$%&*{}()]|(?=.*\d)).{7,}/;
       validPassword = regExp.test(password);
       if (!validPassword) {
         return error_msg;
@@ -463,8 +463,8 @@ angular.module('core').directive('teacherAccess', function (identity) {
     // Valid chars for user passwords
     this.validCharPwd = function(password) {
       var error_msg, regExp, validPassword;
-      error_msg = gettext('Password is not valid. Password can only contains a-zA-Z0-9?!@#§+\-$%&*{}()\]\[');
-      regExp = /^[a-zA-Z0-9?!@#§+\-$%&*{}()\]\[]+$/;
+      error_msg = gettext('Password is not valid. Password can only contains a-zA-Z0-9.?!@#§+\-$%&*{}()\]\[');
+      regExp = /^[a-zA-Z0-9\.?!@#§+\-$%&*{}()\]\[]+$/;
       validPassword = regExp.test(password);
       if (!validPassword) {
         return error_msg;

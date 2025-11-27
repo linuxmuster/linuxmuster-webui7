ab9a0d343ac7931da72aab18db50df6b93afdbe1
kiarn <arnaud@linuxmuster.net>
2025-11-27 09:28:23 +0100

    Remove deprecated webuiUpload folder.

--- usr/lib/linuxmuster/setup.d/w_webui.py
+++ usr/lib/linuxmuster/setup.d/w_webui.py
@@ -5,7 +5,4 @@ print("* Create Webui Configuration")
 
 subprocess.run(['/usr/lib/linuxmuster-webui/etc/install_scripts/create_aj_cfg.sh'])
 
-print("* Create Webui Upload Folder")
-subprocess.run(['/usr/lib/linuxmuster-webui/etc/install_scripts/create_webuiUploadFolder.sh'])
-
 print("* WebUI Setup Success!")

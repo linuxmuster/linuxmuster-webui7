3c2007800f10536d4df190298459ad56bb9bdba4
kiarn <arnaud@linuxmuster.net>
2026-08-08 16:06:02 +0200

    Delete deprecated install script.

--- usr/lib/linuxmuster/setup.d/w_webui.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import subprocess
-
-print("#### Creating Webui Configuration ....................................... ####")
-
-subprocess.run(['/usr/lib/linuxmuster-webui/etc/install_scripts/create_aj_cfg.sh'])
-
-print("#### WebUI Setup ............................................... Success! ####")

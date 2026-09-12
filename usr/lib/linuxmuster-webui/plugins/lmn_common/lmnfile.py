ea6d0eaf1918ee5867bab2440f129b0e76a08fea
kiarn <arnaud@linuxmuster.net>
2026-09-12 16:40:25 +0200

    Accept negative values.

--- usr/lib/linuxmuster-webui/plugins/lmn_common/lmnfile.py
+++ usr/lib/linuxmuster-webui/plugins/lmn_common/lmnfile.py
@@ -7,6 +7,7 @@ import os.path
 import logging
 import abc
 import csv
+import re
 import magic
 import filecmp
 import time
@@ -318,7 +319,9 @@ class ConfigLoader(LMNFile):
             )
             for section, options in self.data.items():
                 for key, value in options.items():
-                    value = int(value) if value.isdigit() else value
+                    # isdigit() is False for a sign, and -1 is a valid value in
+                    # school.conf (unlimited quota): it has to be converted too.
+                    value = int(value) if re.fullmatch(r'[+-]?\d+', value) else value
                     value = True if value == 'yes' else value
                     value = False if value == 'no' else value
                     self.data[section][key] = value
